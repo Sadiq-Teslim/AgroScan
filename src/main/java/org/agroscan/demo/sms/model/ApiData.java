2ab853e504ad87ccd03a15f4892386d0462e88e3
HenryTech12 <fakorodehenry@gmail.com>
2025-06-03 14:42:18 +0100

    sms integration made

--- src/main/java/org/agroscan/demo/sms/model/ApiData.java
+++ src/main/java/org/agroscan/demo/sms/model/ApiData.java
@@ -1,32 +1,52 @@
 package org.agroscan.demo.sms.model;
 
+import java.io.File;
+
 public class ApiData {
 
-     private Long id;
-     private String message;
-     private String userContact;
+     private String body;
+     private File image;
+     private int NumMedia;
+     private String mediaUrl0;
+     private String from;
 
-      public Long getId() {
-    return id;
-}
+    public String getBody() {
+        return body;
+    }
 
-public void setId(Long id) {
-    this.id = id;
-}
+    public void setBody(String body) {
+        this.body = body;
+    }
 
-public String getMessage() {
-    return message;
-}
+    public File getImage() {
+        return image;
+    }
 
-public void setMessage(String message) {
-    this.message = message;
-}
+    public void setImage(File image) {
+        this.image = image;
+    }
 
-public String getUserContact() {
-    return userContact;
-}
+    public int getNumMedia() {
+        return NumMedia;
+    }
 
-public void setUserContact(String userContact) {
-    this.userContact = userContact;
-}
+    public void setNumMedia(int numMedia) {
+        NumMedia = numMedia;
+    }
+
+    public String getMediaUrl0() {
+        return mediaUrl0;
+    }
+
+    public void setMediaUrl0(String mediaUrl0) {
+        this.mediaUrl0 = mediaUrl0;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
 }
